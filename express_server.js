391dd28bbfa3c317fab0f770b1b24da2c4b2862c
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-26 15:02:32 +0000

    add urls_index.ejs file

--- express_server.js
+++ express_server.js
@@ -8,9 +8,16 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+////////////////////////////////////////////////////////////////////////////////
+// Route Handlers
+////////////////////////////////////////////////////////////////////////////////
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+// this is a reference........... check compatability with the urls.json beneath
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -20,6 +27,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+////////////////////////////////////////////////////////////////////////////////
+// Listener
+////////////////////////////////////////////////////////////////////////////////
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
