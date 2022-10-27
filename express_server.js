35e54767a3613047a6a655882afef4bb40dd8113
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-27 14:42:46 +0000

    fix longURL sent to urls_show

--- express_server.js
+++ express_server.js
@@ -44,7 +44,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[this.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
